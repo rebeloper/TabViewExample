86687ab5dd76bdb5af1e6feff1aaf6e234b70af3
Alex Nagy <therebeloper@gmail.com>
2022-03-17 16:35:32 +0200

    Initial Commit

---
+++ Shared/TabViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewExampleApp.swift
+//  Shared
+//
+//  Created by Alex Nagy on 17.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TabViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
